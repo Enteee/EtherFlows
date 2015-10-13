17eb304b6b34434e9e84f5d3468aab1718b44e54
Neo <noe.thalheim@ruag.com>
2015-10-13 12:55:11 +0200

    Add logstash support to flowworker

--- flowworker/flowworker.py
+++ flowworker/flowworker.py
@@ -32,22 +32,6 @@ class PcapPacket:
         sys.stdout.write(struct.pack("IIII",self.sec, self.usec, self.caplen, self.wirelen))
         sys.stdout.write(str(self.payload))
         sys.stdout.flush()
-        #print(self.payload)
-
-class Flow:
-    def __init__(self, dst):
-        self.pkts = []
-        self.dst_hash = dst
-        self.last_pkt = time.time()
-    def write(self):
-        if len(self.pkts) == 1:
-            #print("Only init pkt recived, flow with dst_hash: {} not written".format(self.dst_hash))
-            return
-
-        for pkt in self.pkts:
-            pkt.write()
-        #print("Flow with dst_hash: {} written, included {} packets".format(self.dst_hash,i))
-
 
 def packet_handler(pkt):
    #check for ether 
@@ -58,50 +42,14 @@ def packet_handler(pkt):
        src = str(pkt.src)
        if dst not in flows:
            sendp(Ether(dst=src, src=dst)/Raw("ENTE"),iface=interface,verbose=False)
-           f = Flow(dst)
-           flows[dst] = f
-       flows[dst].pkts.append(pcap)
-       flows[dst].last_pkt = time.time()
-
-def flow_handler():
-    to_remove = []
-    act_time = time.time()
-    for dst in flows:
-       #check timeout
-       if act_time - flows[dst].last_pkt > FLOW_TIMEOUT:
-           flows[dst].write()
-           to_remove.append(dst)
-
-    for dst in to_remove:
-        del flows[dst]
-
-
-def threaded_sniff_target(q):
-    while (running):
-        sniff(iface=interface, filter="ether src host {0}".format(FLOWGEN_MAC), prn = lambda x : q.put(x), timeout=1)
-
-def threaded_handler(q):
-  while (running):
-    try:
-      pkt = q.get(timeout = 1)
-      packet_handler(pkt)
-    except Empty:
-      #no new pkts so we have time to handle the flows
-      flow_handler()
-      #print("Amount of active flows: {}".format(len(flows)))
-
+           flows[dst] = True
+       pcap.write()
 
 def print_usage():
     print("Usage: ./fworker interface")
     sys.exit(1)
 
-def SIGINT_handler(x,y):
-    global running
-    running = False
-
 if __name__ == "__main__":
-    q = Queue.Queue(maxsize=MAX_PKT)
-
     flows={}    
     if len(sys.argv) != 2:
         print_usage()
@@ -114,19 +62,10 @@ if __name__ == "__main__":
         shutil.rmtree(interface)
     
     os.mkdir(interface)
-    #print("Start sniffing on Port "+interface)
-    #global hdr
+    #global pcap hdr
     hdr = struct.pack("IHHIIII", 0xa1b2c3d4L, 2, 4, 0, 0, MTU, 1)
     sys.stdout.write(hdr)
     sys.stdout.flush()
 
-    sniffer = Thread(target = threaded_sniff_target, args = (q,))
-    sniffer.start()
-    handler = Thread(target = threaded_handler, args = (q,))
-    handler.start()
+    sniff(iface=interface, filter="ether src host {0}".format(FLOWGEN_MAC), prn = packet_handler)
 
-    signal.signal(signal.SIGINT, SIGINT_handler)
-    signal.signal(signal.SIGTERM, SIGINT_handler)
-
-    while running:
-        signal.pause()
--- flowworker/sys/bootstrap_privileged.sh
+++ flowworker/sys/bootstrap_privileged.sh
@@ -4,3 +4,7 @@ pacman --noconfirm -S jdk7-openjdk screen rxvt-unicode-terminfo wireshark-cli li
 
 # Ruby bundler
 gem install bundler
+
+#Wireshark non root config
+gpasswd -a vagrant wireshark
+newgrp wireshark
