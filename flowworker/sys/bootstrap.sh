fee0cba5fa8380a4684d45447c1e9e043f72c177
ente <ducksource@duckpond.ch>
2015-10-14 15:05:48 +0200

    new vagrant file

--- flowworker/sys/bootstrap.sh
+++ flowworker/sys/bootstrap.sh
@@ -38,17 +38,3 @@ tar -xvzf "${LOGSTASH_TAR}" -C "${SERVICES}"
 wget "https://download.elastic.co/kibana/kibana/${KIBANA_TAR}"
 tar -xvzf "${KIBANA_TAR}" -C "${SERVICES}"
 
-##
-# Start services
-##
-screen -d -m -S worksession
-export SCREEN_SESSION=$(screen -ls | sed -nre 's/\s*([0-9]+\.worksession).*/\1/p')
-
-screen -S ${SCREEN_SESSION} -X screen -t "elasticsearch" 
-screen -S ${SCREEN_SESSION} -p "elasticsearch" -X stuff $"${ELASTIC_HOME}/bin/elasticsearch\n"
-
-screen -S ${SCREEN_SESSION} -X screen -t "logstash"
-screen -S ${SCREEN_SESSION} -p "logstash" -X stuff $"${LOGSTASH_HOME}/bin/logstash -f /vagrant/logstash.conf\n"
-
-screen -S ${SCREEN_SESSION} -X screen -t "kibana"
-screen -S ${SCREEN_SESSION} -p "kibana" -X stuff $"${KIBANA_HOME}/bin/kibana\n"
