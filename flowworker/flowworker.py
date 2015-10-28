caadb286f771d11a475acc2fd21dc23fe3942d70
Ente <ducksource@duckpond.ch>
2015-10-28 17:38:59 +0100

    [wip] fworker push

--- flowworker/flowworker.py
+++ flowworker/flowworker.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python2
 # vim: set fenc=utf8 ts=4 sw=4 et :
+import time 
 import sys
 import json
 import argparse
@@ -9,6 +10,7 @@ import socket
 #MAC Addr of the flow generator 
 DATA_MAXLEN = 200
 DATA_TOO_LONG = 'Data too long'
+FLOW_BUFFER_TIME = 3
 
 parser = argparse.ArgumentParser(description='Flowworker')
 
@@ -19,7 +21,7 @@ parser.add_argument('-i',
                     help='Interface to listen on'
                     )
 parser.add_argument('-no',
-                    dest='no_answer',
+                    dest='no_ack',
                     action="store_true",
                     help='Dont answer packets'
                     )
@@ -29,44 +31,61 @@ parser.add_argument('-l',
                     dest='data_maxlen',
                     help='Maximum lenght of data in tshark pdml-field [default: {}]'.format(DATA_MAXLEN)
                     )
+parser.add_argument('-t',
+                    default=FLOW_BUFFER_TIME,
+                    type=int,
+                    dest='flow_buffer_time',
+                    help='Lenght (in seconds) to buffer a flow before writing the packets [default: {}]'.format(FLOW_BUFFER_TIME)
+                    )
 class Flow():
-    def __init__(self, flowid):
-        self.__flowid = flowid
-        self.__pkts = []
-        self.__flused = False
+    def __init__(self, first_frame):
+        self.__flowid = first_frame['eth.dst']
+        self.__flowgen = first_frame['eth.src']
+        self.__frames = [first_frame]
+        self.__flushed = False
+        self.__newest_frame_time = self.__first_frame_time = first_frame['frame.time_epoch']
+        self.__send_ack()
 
-    def add_pkt(self, pkt)
-        pkt['env.flowid'] = self.flowid
+    def add_frame(self, frame):
+        frame['env.flowid'] = self.__flowid
+        # check if packet expands flow length
+        self.__first_frame_time = min(self.__first_frame_time, frame['frame.time_epoch'])
+        self.__newest_frame_time = max(self.__newest_frame_time, frame['frame.time_epoch'])
         if self.__flushed:
-            self._write(pkt)
+            self._write_frame(frame)
         else:
             # Buffer packet
-            self.__pkts.append(pkt)
+            self.__frames.append(frame)
+            flow_length = self.__newest_frame_time - self.__first_frame_time
+            if flow_length > args.flow_buffer_time:
+                self.flush()
 
-    def flush(self)
-        for p in self.__pkts:
-            self.write(p)
+    def expired(self):
+        return not (self._flushed and self.__newest_frame_time < time.time() + args.flow_buffer_time)
+
+    def flush(self):
+        for p in self.__frames:
+            self._write_frame(p)
         self.__flused = True
 
-    def _write(self, pkt):
-        json.dump(p,sys.stdout)
+    def __send_ack(self):
+        if not args.no_ack:
+            ack_frame = self.__flowgen.replace(':','').decode('hex') # dst MAC
+            ack_frame += self.__flowid.replace(':','').decode('hex') # src MAC
+            ack_frame += '\x09\x00' # ethertype 
+            ack_frame += 'ENTE' # payload
+            ack_frame += '\x63\x07\x3d\x02' # checksum
+            socket.send(ack_frame)
+
+    def _write_frame(self, frame):
+        json.dump(frame, sys.stdout)
         sys.stdout.write('\n')
         sys.stdout.flush()
 
-            
-
 class PdmlHandler(xml.sax.ContentHandler):
+
     def __init__(self):
-        pass
-
-    def send_ack(self, src, dst):
-        src_hex = src.replace(':','').decode('hex')
-        dst_hex = dst.replace(':','').decode('hex')
-
-        payload = 'ENTE'
-        checksum = '\x63\x07\x3d\x02'
-        ethertype = '\x09\x00'
-        s.send(dst_hex+src_hex+ethertype+payload+checksum)
+        self.__frame = {}
 
     def boolify(self, s):
         if s == 'True':
@@ -87,7 +106,7 @@ class PdmlHandler(xml.sax.ContentHandler):
     # Call when an element starts
     def startElement(self, tag, attributes):
         if tag == 'packet':
-            pkt.clear()
+            self.__frame.clear()
         else:
             if attributes.has_key('name'):
                 name = attributes.getValue('name')
@@ -96,29 +115,27 @@ class PdmlHandler(xml.sax.ContentHandler):
                     show = attributes.getValue('show')
                     if len(show) > args.data_maxlen:
                         show = DATA_TOO_LONG
-                    pkt[name] = self.autoconvert(show)
+                    self.__frame[name] = self.autoconvert(show)
                 # Extract showname
                 if attributes.has_key('showname'):
                     showname = attributes.getValue('showname')
                     if len(showname) > args.data_maxlen:
                         showname = DATA_TOO_LONG
-                    pkt['{}.show'.format(name)] = showname
+                    self.__frame['{}.show'.format(name)] = showname
 
     # Call when an elements ends
     def endElement(self, tag):
         if tag == 'packet':
-            if not args.no_answer:
                 try:
-                    flowid = pkt['eth.dst']
+                    flowid = self.__frame['eth.dst']
                     try: 
-                        flow = flows[flowid]
+                        flows[flowid].add_frame(self.__frame)
                     except KeyError:
-                        flows[flowid] = Flow(flowid)
+                        # flow unknown add new flow
+                        flows[flowid] = Flow(self.__frame)
                         flow = flows[flowid]
-                    flow.add_pkt(pkt)
                 except KeyError:
                     pass
-
 
     # Call when a character is read
     def characters(self, content):
@@ -126,20 +143,11 @@ class PdmlHandler(xml.sax.ContentHandler):
 
 if ( __name__ == '__main__'):
     args = parser.parse_args()
-    if not args.no_answer:
-        flows = {}
-        try:
-            s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-        except socket.error , msg:
-            print('Socket could not be created. Error Code : {} Message: {}'.format(str(msg[0]),msg[1]))
-            sys.exit()
-        try:
-            s.bind((args.interface, 0))
-        except:
-            print('Could not bind socket')
-            sys.exit()
-    #pkt dictionary
-    pkt = {}
+    flows = {}
+    # bind socket
+    if not args.no_ack:
+        socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+        socket.bind((args.interface, 0))
     # create an XMLReader
     parser = xml.sax.make_parser()
     # turn off namepsaces
@@ -148,3 +156,7 @@ if ( __name__ == '__main__'):
     handler = PdmlHandler()
     parser.setContentHandler(handler)
     parser.parse(sys.stdin)
+    # flush all buffered flows
+    for f in flows:
+        
+    
