501de011afa2b87c96826bc820c67b292fbed305
Mischa Lehmann <ducksource@duckpond.ch>
2015-11-26 10:06:02 +0000

    string fields back

--- flowworker/logstash/config/template.js
+++ flowworker/logstash/config/template.js
@@ -8,6 +8,15 @@
     "mappings": {
         "_default_": {
             "_all": { "enabled": true },
+            "dynamic_templates" : [{
+                "string_fields" : {
+                    "match" : "*",
+                    "match_mapping_type" : "string",
+                    "mapping" : {
+                        "type" : "string", "index" : "not_analyzed", "omit_norms" : true
+                    }
+                }
+            }],
             "properties": {
                 "env": {
                     "properties": {
